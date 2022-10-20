89792f22d9e498fc753ce3ece9e06722af278929
gksdudrb922 <gksdudrb922@gmail.com>
2022-10-20 17:16:12 +0900

    [VR-15] Feat: add first project

---
+++ src/main/java/com/vroomvroom/safemobis/SafeMobisApplication.java
@@ -0,0 +1,13 @@
+package com.vroomvroom.safemobis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeMobisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeMobisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vroomvroom/safemobis/SafeMobisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vroomvroom.safemobis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeMobisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
