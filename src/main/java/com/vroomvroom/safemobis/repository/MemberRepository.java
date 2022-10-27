bae5a8e0abd758516df8eb225524c2a98bebd38d
gksdudrb922 <gksdudrb922@gmail.com>
2022-10-27 14:36:47 +0900

    [VR-27] Feat: Override findAll() in JPA
    - findAll() 수행 시, trafficModes와 postion을 같이 fetch join 하도록 override

--- src/main/java/com/vroomvroom/safemobis/repository/MemberRepository.java
+++ src/main/java/com/vroomvroom/safemobis/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import com.vroomvroom.safemobis.domain.Member;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -11,4 +12,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @EntityGraph(attributePaths = {"trafficModes", "position"})
     Optional<Member> findByUsername(String username);
 
+    @Override
+    @EntityGraph(attributePaths = {"trafficModes", "position"})
+    List<Member> findAll();
 }
