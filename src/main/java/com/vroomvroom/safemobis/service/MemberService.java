8a366579afdfd92fe13426aa4513c5ebe7f3ae1a
gksdudrb922 <gksdudrb922@gmail.com>
2022-11-13 23:59:39 +0900

    [VR-37] Feat: Add members intersection get api in MemberController
    - 회원의 모든 교차점 조회

--- src/main/java/com/vroomvroom/safemobis/service/MemberService.java
+++ src/main/java/com/vroomvroom/safemobis/service/MemberService.java
@@ -1,20 +1,20 @@
 package com.vroomvroom.safemobis.service;
 
-import com.vroomvroom.safemobis.domain.*;
-import com.vroomvroom.safemobis.domain.Position;
-import com.vroomvroom.safemobis.domain.enumerate.TrafficCode;
-import com.vroomvroom.safemobis.domain.enumerate.WarningCode;
-import com.vroomvroom.safemobis.dto.response.member.MembersWarningGetResponseDto;
+import com.vroomvroom.safemobis.domain.Intersection;
+import com.vroomvroom.safemobis.domain.Member;
+import com.vroomvroom.safemobis.domain.Path;
+import com.vroomvroom.safemobis.domain.PathIntersection;
+import com.vroomvroom.safemobis.dto.response.member.MembersIntersectionsGetResponseDto;
 import com.vroomvroom.safemobis.dto.response.member.TokenInfo;
 import com.vroomvroom.safemobis.error.exception.EntityAlreadyExistException;
 import com.vroomvroom.safemobis.error.exception.EntityNotFoundException;
 import com.vroomvroom.safemobis.repository.MemberRepository;
 import com.vroomvroom.safemobis.repository.PathRepository;
-import com.vroomvroom.safemobis.security.SecurityUtil;
 import com.vroomvroom.safemobis.security.jwt.JwtTokenProvider;
-import com.vroomvroom.safemobis.util.Circle;
 import lombok.RequiredArgsConstructor;
-import org.locationtech.jts.geom.*;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.geom.Point;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -23,11 +23,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import static com.vroomvroom.safemobis.domain.enumerate.WarningCode.SAFE;
-import static com.vroomvroom.safemobis.domain.enumerate.WarningCode.WARN;
-import static com.vroomvroom.safemobis.util.CircleUtil.getRadiusMap;
+import static com.vroomvroom.safemobis.security.SecurityUtil.getCurrentUsername;
 
 @Service
 @Transactional(readOnly = true)
@@ -57,6 +55,11 @@ public class MemberService {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
         return jwtTokenProvider.generateToken(authentication);
+    }
+
+    public Member findByUsername(String username) {
+        return memberRepository.findByUsername(username)
+                .orElseThrow(() -> new EntityNotFoundException("[" + username + "] 회원가입이 되어있지 않습니다."));
     }
 
     @Transactional
@@ -103,60 +106,22 @@ public class MemberService {
                 .build());
     }
 
-    public Member findByUsername(String username) {
-        return memberRepository.findByUsername(username)
-                .orElseThrow(() -> new EntityNotFoundException("[" + username + "] 회원가입이 되어있지 않습니다."));
+    public MembersIntersectionsGetResponseDto getIntersections() {
+        Member member = findByUsername(getCurrentUsername());
+        List<Intersection> intersections = getIntersections(member);
+        return MembersIntersectionsGetResponseDto.from(intersections);
     }
 
-    @Transactional
-    public void updatePosition(Position updatePosition) {
-        Member member = findByUsername(SecurityUtil.getCurrentUsername());
-        member.updatePosition(updatePosition);
-    }
-
-    public MembersWarningGetResponseDto getSurroundMembersAndWarning() throws Exception {
-        Member member = findByUsername(SecurityUtil.getCurrentUsername());
-        List<Member> surroundingMembers = getSurroundingMembers(member);
-        List<Member> warningMembers = getWarningMembers(member, surroundingMembers);
-        WarningCode warningCode = warningMembers.size() > 0 ? WARN : SAFE;
-        return MembersWarningGetResponseDto.of(surroundingMembers, warningCode, warningMembers);
-    }
-
-    private List<Member> getSurroundingMembers(Member member) throws Exception {
-        final double RADIUS = 50.0;
-        Map<TrafficCode, Boolean> trafficWarningMap = member.getTrafficWarningMap();
-        Position position = member.getPosition();
-        List<Member> members = memberRepository.findAll();
-        List<Member> surroundingMembers = new ArrayList<>();
-        for (Member surroundingMember : members) {
-            if (member.getId().equals(surroundingMember.getId())) {
-                continue;
-            }
-            Circle circle = Circle.of(position.getX(), position.getY(), RADIUS);
-            Position surroundingPosition = surroundingMember.getPosition();
-            if (trafficWarningMap.get(surroundingMember.getTrafficCode())
-                    && circle.isPositionInCircle(surroundingPosition.getX(), surroundingPosition.getY())) {
-                surroundingMembers.add(surroundingMember);
+    private List<Intersection> getIntersections(Member member) {
+        List<Intersection> intersections = new ArrayList<>();
+        List<Path> paths = pathRepository.findByMemberWithIntersection(member);
+        for (Path path : paths) {
+            List<PathIntersection> pathIntersections = path.getPathIntersections();
+            for (PathIntersection pathIntersection : pathIntersections) {
+                intersections.add(pathIntersection.getIntersection());
             }
         }
-        return surroundingMembers;
+        return intersections;
     }
 
-    private static List<Member> getWarningMembers(Member member, List<Member> surroundingMembers) {
-        final double TIME = 3.0;
-        Map<TrafficCode, Double> radiusMap = getRadiusMap();
-        Position beforePosition = member.getPosition();
-        Position afterPosition = beforePosition.getPositionAfter(TIME);
-        Circle circle = Circle.of(afterPosition.getX(), afterPosition.getY(), radiusMap.get(member.getTrafficCode()));
-        List<Member> warningMembers = new ArrayList<>();
-        for (Member surroundingMember : surroundingMembers) {
-            Position beforeSurroundingPosition = surroundingMember.getPosition();
-            Position afterSurroundingPosition = beforeSurroundingPosition.getPositionAfter(TIME);
-            Circle surroundingCircle = Circle.of(afterSurroundingPosition.getX(), afterSurroundingPosition.getY(), radiusMap.get(surroundingMember.getTrafficCode()));
-            if (circle.isOverlap(surroundingCircle)) {
-                warningMembers.add(surroundingMember);
-            }
-        }
-        return warningMembers;
-    }
 }
